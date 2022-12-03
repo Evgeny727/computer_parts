64ad351013a2b2415a685fb91f2ec930ebd7cce0
Evgeny <genyakarelin23@gmail.com>
2022-12-03 20:18:43 +0300

    Init

--- first.js
+++ first.js
@@ -1,6 +1,6 @@
 let mass_cards = document.querySelectorAll(".product-card"),flag_display = false;
 
-mass_cards.forEach(function callback(element, index) {
+mass_cards.forEach(function callback(element) {
     element.addEventListener('click', function() {
         if(flag_display === false) {
             element.querySelector(".product-parametrs").style.display = "block";
